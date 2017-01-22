a77524926092f883a21cc72a5ad4d6f1256ca407
Mikko Hirvonen <hirvonenmp@gmail.com>
2017-01-22 12:19:11 +0200

    Add project files.

---
+++ Teht_01/Kiuas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Tehtävänäsi on ohjelmoida kiukaan toiminta.
+ *  Kiuas pitää pystyä laittamaan päälle ja pois, sekä sen lämpötilaa että sen antamaa kosteutta
+ *   pitää pystyä säätämään (arvoja ei ole rajattu).
+UML
+Suunnittele Kiuas-luokan ominaisuudet ja toiminnot UML-luokkakaaviona.
+Ohjelmointi
+Toteuta Kiuas-luokan ohjelmointi sekä pääohjelma, jolla luot olion Kiuas-luokasta.
+Säädä kiuas-oliota erilaisilla arvoilla, jätä Console.WriteLine()-tulostuslauseet ohjelmaasi,
+jotta kiuas-olion käyttäminen jää näkyville.
+ */
+namespace Teht_01
+{
+    class Kiuas
+    {
+        //properties
+        public Boolean OnOff { get; set; }
+        public int Temperature { get; set; }
+        public  int Moiseture { get; set; }
+        // method
+        
+        public void KiuasOn()
+        {
+            OnOff = true;
+        }
+        public void KiuasOff()
+        {
+            OnOff = false;
+        }
+        public void MoreHeat()
+        {
+            Temperature += 5;
+        }
+        public void LessHeat()
+        {
+            Temperature -= 5;
+        }
+        public void MoreSteam()
+        {
+            Moiseture += 3;
+        }
+        public void LessSteam()
+        {
+            Moiseture -= 3;
+        }
+    }
+}
---
+++ Teht_01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas harvia = new Kiuas();
+            harvia.KiuasOn();
+            harvia.Moiseture = 50;
+            harvia.Temperature = 70;
+            Console.WriteLine(harvia.OnOff);
+            Console.WriteLine(harvia.Moiseture);
+            Console.WriteLine(harvia.Temperature);
+            Console.ReadKey();
+            // Lisätään lämpöä ja vähennetään kosteutta
+            harvia.MoreHeat();
+            harvia.LessSteam();
+            Console.WriteLine();
+            Console.WriteLine(harvia.OnOff);
+            Console.WriteLine(harvia.Moiseture);
+            Console.WriteLine(harvia.Temperature);
+            Console.ReadKey();
+            // vähennetään lämpöa ja lisätään kosteutta
+            harvia.LessHeat();
+            harvia.MoreSteam();
+            Console.WriteLine();
+            Console.WriteLine(harvia.OnOff);
+            Console.WriteLine(harvia.Moiseture);
+            Console.WriteLine(harvia.Temperature);
+            Console.ReadKey();
+            // Pois päältä
+            harvia.KiuasOff();
+            Console.WriteLine(harvia.OnOff);
+            Console.ReadKey();
+        }
+    }
+}
