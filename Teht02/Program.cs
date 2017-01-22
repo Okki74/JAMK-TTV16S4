a38ce6578c64a940d64e5f2a9ef198d0fe631e28
Mikko Hirvonen <hirvonenmp@gmail.com>
2017-01-22 14:35:33 +0200

    Tehtävä 2 lisätty

--- Teht02/Program.cs
+++ Teht02/Program.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*Tehtävänäsi on ohjelmoida pesukoneen toiminta. Samoin kuin edellinen tehtävä: mitä ominaisuuksia ja
+ *  toimintoja tekisit Pesukone-luokkaan?
+UML
+Suunnittele Pesukone-luokan ominaisuudet ja toiminnot UML-luokkakaaviona.
 
+Ohjelmointi
+
+Toteuta Pesukone-luokan ohjelmointi sekä pääohjelma,
+jolla luot olion Pesukone-luokasta. Säädä pesukone-oliota erilaisilla
+arvoilla, jätä Console.WriteLine()-tulostuslauseet ohjelmaasi, jotta pesukone-olion
+käyttäminen jää näkyville. Toteuta Pesukone-luokkaan muutamia erilaisia konstruktoreita
+ja alusta niitä käyttämällä oliota pääohjelmasta käsin.
+ */
 namespace Teht02
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //WasingMa chineshit = new WasingMa();
+            
+            WasingMa upo = new WasingMa(40, 1200, 60);
+            upo.OnOff = true;
+            WasingMa hoover = new WasingMa(2, 800);
+            hoover.SetTemp(35);
+            hoover.StarProgram();
+            WasingMa aeg = new WasingMa(55, 1500, 9, true);
+            Console.WriteLine("Upo");
+            upo.Print();
+            Console.WriteLine("Hoover");
+            hoover.Print();
+            Console.WriteLine("AEG");
+            aeg.Print();
+            Console.ReadKey();
+            aeg.StopProgram();
+            aeg.Print();
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
--- Teht02/WasingMa.cs
+++ Teht02/WasingMa.cs
@@ -8,5 +8,60 @@ namespace Teht02
 {
     class WasingMa
     {
+        //properties
+        public int WatherTemp { get; set; }
+        public int Speed { get; set; }
+        public int Time { get; set; }
+        public int ProgNum { get; set; }
+        public bool OnOff { get; set; }
+        // constructors
+        public WasingMa(int temp, int speed, int progNum, bool on)
+        {
+            WatherTemp = temp;
+            Speed = speed;
+            ProgNum = progNum;
+            OnOff = on;
+        }
+        public WasingMa(int temp, int speed, int time)
+        {
+            WatherTemp = temp;
+            Speed = speed;
+            Time = time;
+        }
+        public WasingMa(int progNum, int speed)
+        {
+
+            Speed = speed;
+            ProgNum = progNum;
+        }
+        // methods
+        public void StarProgram(){
+            OnOff = true;
+        }
+        public void StopProgram(){
+            OnOff = false;
+        }
+        public void SetTemp(int temp){
+            WatherTemp = temp;
+        }
+        public void SetSpeed(int speed){
+            Speed = speed;
+        }
+        public void SetProg(int num){
+            ProgNum = num;
+        } 
+        public void Print()
+        {
+            Console.WriteLine("Vedenlämpötila: " + WatherTemp);
+            Console.WriteLine("Linkous: " + Speed);
+            Console.WriteLine("Ajastin : " + Time);
+            Console.WriteLine("Ohjelma numero " + ProgNum);
+            Console.WriteLine("Kone on päällä: " + OnOff);
+
+        }
+      
+
+
+
     }
 }
