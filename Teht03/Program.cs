154d7297177ca2e524a9458a2d4fd0e217d6b9e8
Mikko Hirvonen <hirvonenmp@gmail.com>
2017-01-22 15:15:06 +0200

    Tehtävä 3

---
+++ Teht03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Tehtävä 3
+Tehtävänäsi on ohjelmoida television toiminta. Samoin kuin edellinen tehtävä: mitä ominaisuuksia
+ja toimintoja tekisit Televisio-luokkaan?
+UML
+Suunnittele Televisio-luokan ominaisuudet ja toiminnot UML-luokkakaaviona.
+
+Ohjelmointi
+Toteuta Televisio-luokan ohjelmointi sekä pääohjelma, jolla luot olion Televisio-luokasta.
+Säädä luomaasi Televsio-oliota erilaisilla arvoilla, jätä Console.WriteLine()-tulostuslauseet ohjelmaasi,
+jotta televisio-olion käyttäminen jää näkyville
+ */
+
+namespace Teht03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tv salora = new Tv(true, 20, 50, 1);
+            salora.Print();
+            Console.ReadKey();
+            salora.Brightness = 30;
+            salora.VolumeDown();
+            salora.SetTextTv();
+            salora.Print();
+            Console.ReadKey();
+            salora.SetTextTv();
+            salora.BrightnessUp();
+            salora.ChangeChannel(99);
+            salora.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Teht03/Tv.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht03{
+    class Tv{
+        //properties
+        public Boolean OnOff { get; set; }
+        public int Volume { get; set; }
+        public int Chanel { get; set; }
+        public int Brightness { get; set; }
+        public bool TextTv { get; set; }
+
+        // constructors
+        public Tv( bool on, int volume, int brightness, int chanel){
+            OnOff = on;
+            Volume = volume;
+            Brightness = brightness;
+            Chanel = chanel;
+        }
+        // methods
+        public void ChangeChannel (int chanel){
+            Chanel = chanel;
+        } 
+        public void VolumeUp(){
+            Volume += 5;
+        }
+        public void VolumeDown(){
+            Volume -= 5;
+        }
+        public void BrightnessUp(){
+            Brightness += 5;
+        }
+        public void BrightnessDown()
+        {
+            Brightness -= 5;
+        }
+        public void SetTextTv(){
+            if (TextTv == false){
+                TextTv = true;
+            }
+            else { TextTv = false; }
+        }
+        public void Print(){
+            Console.WriteLine("KANAVA: " + Chanel); // Tehtävän annossa sanottu Console....
+            Console.WriteLine("VOLUME: " + Volume);
+            Console.WriteLine("KIRKKAUS: " + Brightness);
+            Console.WriteLine("TEKSTITV" + TextTv);
+            Console.WriteLine("Kone on päällä: " + OnOff);
+        }
+    }
+}
